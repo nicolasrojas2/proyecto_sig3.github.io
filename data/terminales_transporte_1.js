a405787b15cc58d8b41906be5e6a876f3e6affc0
nicolasrojas2 <35353801+nicolasrojas2@users.noreply.github.com>
2019-03-31 01:29:04 +0300

    Add files via upload

---
+++ data/terminales_transporte_1.js
@@ -0,0 +1,8 @@
+var json_terminales_transporte_1 = {
+"type": "FeatureCollection",
+"name": "terminales_transporte_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "gid": 1.0, "nombre": "TERMINAL DE TRANSPORTE TERRESTRE CALI", "direccion": null, "telefono": "6683655-6673761", "municipio": "CALI", "horario": null, "categoria": null, "marca": null }, "geometry": { "type": "Point", "coordinates": [ -76.521192757999927, 3.464971864000063 ] } }
+]
+}
